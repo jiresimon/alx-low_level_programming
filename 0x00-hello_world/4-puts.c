1e62949df0a7061d25546357d160c571bbf23de8
jiresimon <simondolapo@gmail.com>
2021-08-26 16:32:31 +0000

    Add comments to tas4 file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main()
+/**
+  * main - prints programming is like building a multilingual puzzle
+  *
+  *Return: Always 0 (Success)
+  *
+  */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
