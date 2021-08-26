b1912fa151d28de53107b39e4783f2ee2ff7df61
jiresimon <simondolapo@gmail.com>
2021-08-26 20:40:20 +0000

    Modify the no of characters in task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <string.h>
 /**
-  *main - Prints "and that piece of art is useful" - Dora Korpar\
+  * main - Prints "and that piece of art is useful" - Dora Korpar\
   2015-10-19 followed by a newline, to standard error.
   *
   * Return: Always 1
