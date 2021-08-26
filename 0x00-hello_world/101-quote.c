c26429f1e3164ff8cd2df82c65fe8da30d676332
jiresimon <simondolapo@gmail.com>
2021-08-26 20:26:29 +0000

    Write the task8 C file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string,h>
+/**
+  *main - Prints "and that piece of art is useful" - Dora Korpar\
+  2015-10-19 followed by a newline, to standard error.
+  *
+  * Return: Always 1
+  */
+int main(void)
+{
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      s);
+
+	return (1);
+}
+
