5826af2562ddc85f1ad03b2da07df91e33fc81aa
jiresimon <simondolapo@gmail.com>
2021-09-01 13:20:37 +0000

    Create the task2 file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_alphabet - prints the alphabet 10times in lowercase.
+  *
+  * Return: Always 0
+  */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		for (letter = 'a'; letter <= 'z'; ++letter)
+			_putchar(letter);
+	}
+	
+	_putchar('\n');
+}
