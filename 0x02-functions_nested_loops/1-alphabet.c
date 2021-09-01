fac7ddda9085054e637a865a2a2e4b2d39ee3433
jiresimon <simondolapo@gmail.com>
2021-09-01 12:46:01 +0000

    Create the task1 file1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * main - prints the alphabet in lowercase.
+  *
+  * Return: Always 0
+  */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; ++letter)
+	{
+		_putchar(letter);
+	}
+
+	putchar('\n');
+}
