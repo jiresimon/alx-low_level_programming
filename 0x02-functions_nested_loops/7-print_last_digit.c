adc4f32abe6465d92ccb5cdb7619064fe9decbde
jiresimon <simondolapo@gmail.com>
2021-09-01 15:23:56 +0000

    Modify task7 file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,18 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int nv;
 
-	if (last_digit < 10)
-		last_digit *= -1;
-
-	_putchar(last_digit + '0');
-
-	return (last_digit);
+	if (n < 0)
+	{
+		nv = -1 * (n % 10);
+		_putchar(nv + '0');
+		return (nv);
+	}
+	else
+	{
+		nv = n % 10;
+		_putchar(nv + '0');
+		return (nv);
+	}
 }
